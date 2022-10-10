2341a3f4cd78a2206ee13b8a48968eb8d51e6f63
Anastasya D <itdolmatova@gmail.com>
2022-10-10 03:42:12 +0300

    <refact>: add 1 edit.navigation&header

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './Navigation.css';
 import burgerPath from '../../images/navi-burger.svg';
 import profilePath from '../../images/profile-icon.svg';
-import closeButtonPath from '../../images/close-button.svg';
+import closeButtonPath from '../../images/burger_close-button.svg';
 
 function Navigation(props) {
 
@@ -14,25 +14,25 @@ function Navigation(props) {
                     <Link to="movies" className="navigation__movies_link">Фильмы</Link>
                     <Link to="saved" className="navigation__movies_link">Сохраненные фильмы</Link>
                 </div>
-                <div>
-                    <Link to="profile" className="navigation__profile">
-                        <h2 className="navigation__profile_link">Аккаунт</h2>
-                        <img className="navigation__profile-icon" alt="Логотип" src={profilePath} />
-                    </Link>
-                </div>
+                <Link to="profile" className="navigation__profile">
+                    <h2 className="navigation__profile_link">Аккаунт</h2>
+                    <img className="navigation__profile-icon" alt="Логотип профиля" src={profilePath} />
+                </Link>
             </div>
+            <img className="navigation__burger_icon" alt="Логотип" src={burgerPath} />
             <div className="navigation__burger">
-                <img className="navigation__burger_icon" alt="Логотип" src={burgerPath} />
-                <div className="navigation__menu">
-                    <div>
-                    <Link to="" className="navigation__menu_link">Главная</Link>
-                    <Link to="movies" className="navigation__menu_link">Фильмы</Link>
-                    <Link to="saved" className="navigation__menu_link">Сохраненные фильмы</Link>
-                    </div>
-                    <button className="navigation__burger_close-button" src={closeButtonPath} />
+                <div className="navigation__links">
+                    <Link to="" className="navigation__link">Главная</Link>
+                    <Link to="movies" className="navigation__link">Фильмы</Link>
+                    <Link to="saved" className="navigation__link">Сохраненные фильмы</Link>
                 </div>
+                <Link to="profile" className="navigation__profile">
+                    <h2 className="navigation__profile_link">Аккаунт</h2>
+                    <img className="navigation__profile-icon" alt="Логотип" src={profilePath} />
+                </Link>
+                <button className="navigation__close-button" src={closeButtonPath} />
             </div>
-        </div>
+        </div >
     );
 }
 
