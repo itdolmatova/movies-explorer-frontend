46bad4d759e3432005b20bb97d6f602b53e564b8
Anastasya D <itdolmatova@gmail.com>
2022-10-24 01:00:10 +0300

    <refact>:edit app.js

--- src/components/App.js
+++ src/components/App.js
@@ -43,7 +43,7 @@ function App() {
           </Route>
 
           <Route exact path="/saved">
-            <SavedMovies />
+            <SavedMovies errorHandler={handleError}/>
           </Route>
 
           <Route exact path="/profile">
